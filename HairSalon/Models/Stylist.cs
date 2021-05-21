d1a2b0a482640c3967edcc503844c7923f3ff2f7
Patrick Lee <patricklee1138@gmail.com>
2021-05-21 16:17:06 -0700

    add Delete functionality to Clients

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,20 +1,3 @@
-// using System.Collections.Generic;
-
-// namespace HairSalon.Models
-// {
-//   public class Stylist
-//   {
-//     public Stylist()
-//     {
-//       this.Clients = new HashSet<Client>();
-//     }
-
-//     public int StylistId { get; set; }
-//     public string NameFirst { get; set; }
-//     public virtual ICollection<Client> Clients { get; set; }
-//   }
-// }
-
 using System.Collections.Generic;
 
 namespace HairSalon.Models
